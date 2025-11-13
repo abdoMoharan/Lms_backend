eed3d9ac921624412d3fc199d5fe6ba37d8a4c91
Abd Elrhmaan <elrhmaana201@gmail.com>
2025-11-13 10:44:42 +0200

    fix(cors): remove '/en' from allowed origins paths

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000','https://lms-test-2002.vercel.app','https://admin-lms-test.vercel.app/en','https://teacher-lms-test-2002.vercel.app/en'],
+    'allowed_origins' => ['http://localhost:3000','https://lms-test-2002.vercel.app','https://admin-lms-test.vercel.app','https://teacher-lms-test-2002.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
